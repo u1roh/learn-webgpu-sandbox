a8478ca71b442f7380508cc516d8c65c1a09f909
Yuichiro Terada <yuichiro_terada@caddi.jp>
2022-08-14 20:08:46 +0900

    simplify

--- rust-desktop/src/lib.rs
+++ rust-desktop/src/lib.rs
@@ -4,11 +4,6 @@ use winit::{
     window::{Window, WindowBuilder},
 };
 
-#[cfg_attr(target_arc = "wasm32", wasm_bindgen)]
-pub fn hello() {
-    log::info!("hello")
-}
-
 #[cfg_attr(target_arch = "wasm32", wasm_bindgen(start))]
 pub async fn run() {
     cfg_if::cfg_if! {
@@ -65,12 +60,6 @@ struct State {
 
     vertex_buffer: wgpu::Buffer,
     vertex_count: u32,
-
-    compute_pipeline: wgpu::ComputePipeline,
-    bind_group: wgpu::BindGroup,
-
-    compute_buffer: wgpu::Buffer,
-    staging_buffer: wgpu::Buffer,
 }
 
 impl State {
@@ -160,127 +149,6 @@ impl State {
             usage: wgpu::BufferUsages::VERTEX,
         });
 
-        // create rendering pipeline
-        let compute_shader = device.create_shader_module(wgpu::ShaderModuleDescriptor {
-            label: Some("Shader"),
-            source: wgpu::ShaderSource::Wgsl(include_str!("compute.wgsl").into()),
-        });
-        // let bind_group_layout = device.create_bind_group_layout(&wgpu::BindGroupLayoutDescriptor {
-        //     /// Debug label of the bind group layout. This will show up in graphics debuggers for easy identification.
-        //     label: Some("bind group layout"),
-
-        //     /// Array of entries in this BindGroupLayout
-        //     entries: &[wgpu::BindGroupLayoutEntry {
-        //         /// Binding index. Must match shader index and be unique inside a BindGroupLayout. A binding
-        //         /// of index 1, would be described as `layout(set = 0, binding = 1) uniform` in shaders.
-        //         binding: 1,
-        //         /// Which shader stages can see this binding.
-        //         visibility: wgpu::ShaderStages::COMPUTE,
-        //         /// The type of the binding
-        //         ty: wgpu::BindingType::Buffer {
-        //             ty: wgpu::BufferBindingType::Storage { read_only: false },
-        //             has_dynamic_offset: false,
-        //             min_binding_size: Some(std::num::NonZeroU64::new(100).unwrap()),
-        //             // min_binding_size: None,
-        //         },
-        //         count: None,
-        //     }],
-        // });
-        // let pipeline_layout = device.create_pipeline_layout(&wgpu::PipelineLayoutDescriptor {
-        //     /// Debug label of the pipeline layout. This will show up in graphics debuggers for easy identification.
-        //     label: Some("pipeline layout"),
-        //     /// Bind groups that this pipeline uses. The first entry will provide all the bindings for
-        //     /// "set = 0", second entry will provide all the bindings for "set = 1" etc.
-        //     bind_group_layouts: &[&bind_group_layout],
-        //     /// Set of push constant ranges this pipeline uses. Each shader stage that uses push constants
-        //     /// must define the range in push constant memory that corresponds to its single `layout(push_constant)`
-        //     /// uniform block.
-        //     ///
-        //     /// If this array is non-empty, the [`Features::PUSH_CONSTANTS`] must be enabled.
-        //     push_constant_ranges: &[], // &'a [PushConstantRange],
-        // });
-        let compute_pipeline = device.create_compute_pipeline(&wgpu::ComputePipelineDescriptor {
-            /// Debug label of the pipeline. This will show up in graphics debuggers for easy identification.
-            label: Some("hoge"),
-            /// The layout of bind groups for this pipeline.
-            layout: None, // Option<&'a PipelineLayout>,
-            // layout: Some(&pipeline_layout), // Option<&'a PipelineLayout>,
-            /// The compiled shader module for this stage.
-            module: &compute_shader, // &'a ShaderModule,
-            /// The name of the entry point in the compiled shader. There must be a function that returns
-            /// void with this name in the shader.
-            entry_point: "main", // &'a str,
-        });
-
-        let compute_buffer = device.create_buffer(&wgpu::BufferDescriptor {
-            /// Debug label of a buffer. This will show up in graphics debuggers for easy identification.
-            label: Some("maguro"),
-            /// Size of a buffer.
-            size: BUFFER_SIZE,
-            /// Usages of a buffer. If the buffer is used in any way that isn't specified here, the operation
-            /// will panic.
-            usage: wgpu::BufferUsages::STORAGE | wgpu::BufferUsages::COPY_SRC,
-            /// Allows a buffer to be mapped immediately after they are made. It does not have to be [`BufferUsages::MAP_READ`] or
-            /// [`BufferUsages::MAP_WRITE`], all buffers are allowed to be mapped at creation.
-            mapped_at_creation: true,
-        });
-        {
-            let mut buf = compute_buffer.slice(..).get_mapped_range_mut();
-            buf[0] = 0xff;
-            buf[1] = 0xfe;
-            buf[2] = 0xfd;
-        }
-        compute_buffer.unmap();
-
-        let bind_group = device.create_bind_group(&wgpu::BindGroupDescriptor {
-            /// Debug label of the bind group. This will show up in graphics debuggers for easy identification.
-            label: Some("piyo"),
-            /// The [`BindGroupLayout`] that corresponds to this bind group.
-            // layout: &bind_group_layout, // &'a BindGroupLayout,
-            layout: &compute_pipeline.get_bind_group_layout(0), // &'a BindGroupLayout,
-            /// The resources to bind to this bind group.
-            // entries: &'a [BindGroupEntry<'a>],
-            entries: &[wgpu::BindGroupEntry {
-                /// Slot for which binding provides resource. Corresponds to an entry of the same
-                /// binding index in the [`BindGroupLayoutDescriptor`].
-                binding: 1,
-                /// Resource to attach to the binding
-                resource: wgpu::BindingResource::Buffer(wgpu::BufferBinding {
-                    /// The buffer to bind.
-                    buffer: &compute_buffer,
-                    /// Base offset of the buffer. For bindings with `dynamic == true`, this offset
-                    /// will be added to the dynamic offset provided in [`RenderPass::set_bind_group`].
-                    ///
-                    /// The offset has to be aligned to [`Limits::min_uniform_buffer_offset_alignment`]
-                    /// or [`Limits::min_storage_buffer_offset_alignment`] appropriately.
-                    offset: 0,
-                    /// Size of the binding, or `None` for using the rest of the buffer.
-                    // size: Some(std::num::NonZeroU64::new(200).unwrap()),
-                    size: None
-                })
-            }]
-        });
-
-        let staging_buffer = device.create_buffer(&wgpu::BufferDescriptor {
-            /// Debug label of a buffer. This will show up in graphics debuggers for easy identification.
-            label: Some("saba"),
-            /// Size of a buffer.
-            size: BUFFER_SIZE,
-            /// Usages of a buffer. If the buffer is used in any way that isn't specified here, the operation
-            /// will panic.
-            usage: wgpu::BufferUsages::MAP_READ | wgpu::BufferUsages::COPY_DST,
-            /// Allows a buffer to be mapped immediately after they are made. It does not have to be [`BufferUsages::MAP_READ`] or
-            /// [`BufferUsages::MAP_WRITE`], all buffers are allowed to be mapped at creation.
-            mapped_at_creation: true,
-        });
-        {
-            let mut buf = staging_buffer.slice(..).get_mapped_range_mut();
-            buf[0] = 0xff;
-            buf[1] = 0xfe;
-            buf[2] = 0xfd;
-        }
-        staging_buffer.unmap();
-
         Self {
             surface,
             device,
@@ -299,12 +167,6 @@ impl State {
             // vertex buffer
             vertex_buffer,
             vertex_count: VERTICES.len() as u32,
-
-            compute_pipeline,
-            bind_group,
-
-            compute_buffer,
-            staging_buffer,
         }
     }
 
@@ -363,44 +225,7 @@ impl State {
             render_pass.draw(0..self.vertex_count, 0..1);
         }
 
-        // {
-        //     let mut compute_pass = encoder.begin_compute_pass(&wgpu::ComputePassDescriptor {
-        //         label: Some("Compute Pass"),
-        //     });
-        //     compute_pass.set_pipeline(&self.compute_pipeline);
-        //     compute_pass.set_bind_group(0, &self.bind_group, &[]);
-        //     // compute_pass.dispatch_workgroups(1, 1, 1);
-        //     compute_pass.dispatch_workgroups(BUFFER_SIZE as u32 / 64, 1, 1);
-        // }
-
-        // encoder.copy_buffer_to_buffer(
-        //     &self.compute_buffer,
-        //     0,
-        //     &self.staging_buffer,
-        //     0,
-        //     BUFFER_SIZE,
-        // );
-
         self.queue.submit(std::iter::once(encoder.finish()));
-
-        // {
-        //     let buffer_slice = self.staging_buffer.slice(..);
-        //     let (tx, rx) = futures_intrusive::channel::shared::oneshot_channel();
-        //     buffer_slice.map_async(wgpu::MapMode::Read, move |result| {
-        //         tx.send(result).unwrap();
-        //     });
-        //     self.device.poll(wgpu::Maintain::Wait);
-        //     pollster::block_on(rx.receive()).unwrap().unwrap();
-
-        //     let data = buffer_slice.get_mapped_range();
-        //     println!("data.len() = {}", data.len());
-        //     for i in 0..10 {
-        //         println!("data[{}] = {}", i, data[i]);
-        //     }
-        //     println!();
-        // }
-        // self.staging_buffer.unmap();
-
         output.present();
         Ok(())
     }
@@ -420,18 +245,6 @@ impl Vertex {
         wgpu::VertexBufferLayout {
             array_stride: std::mem::size_of::<Self>() as wgpu::BufferAddress,
             step_mode: wgpu::VertexStepMode::Vertex,
-            // attributes: &[
-            //     wgpu::VertexAttribute {
-            //         offset: 0,
-            //         shader_location: 0,
-            //         format: wgpu::VertexFormat::Float32x3,
-            //     },
-            //     wgpu::VertexAttribute {
-            //         offset: std::mem::size_of::<[f32; 3]>() as wgpu::BufferAddress,
-            //         shader_location: 1,
-            //         format: wgpu::VertexFormat::Float32x3,
-            //     },
-            // ],
             attributes: &Self::ATTRIBUTES,
         }
     }
@@ -451,5 +264,3 @@ const VERTICES: &[Vertex] = &[
         color: [0.0, 0.0, 1.0],
     },
 ];
-
-const BUFFER_SIZE: u64 = 640;
